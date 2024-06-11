f71d17a6696086537842bab7ce1860fcc83460ca
sublymus <sublymus@gmail.com>
2024-06-11 10:37:29 +0300

    lol

--- fs/products_scene_dir_65b6e94b-b61b-4deb-aa70-dd4485d9b67f/Ring_1/world_config.js
+++ fs/products_scene_dir_65b6e94b-b61b-4deb-aa70-dd4485d9b67f/Ring_1/world_config.js
@@ -6,4 +6,4 @@ export const world_config = {
 	dirPath:"./fs/products_scene_dir_65b6e94b-b61b-4deb-aa70-dd4485d9b67f/Ring_1",
 	dirUrl:"/fs/products_scene_dir_65b6e94b-b61b-4deb-aa70-dd4485d9b67f/Ring_1",
 }
-          
+           
