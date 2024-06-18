860e47f6d6a140275f14ea4c0957c0ae467303e4
sublymus <sublymus@gmail.com>
2024-06-18 16:16:20 +0300

    royal

--- config/database.ts
+++ config/database.ts
@@ -1,5 +1,8 @@
+import env from '#start/env';
 import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
+console.log();
+
 
 const dbConfig = defineConfig({
   connection: 'sqlite',
@@ -7,7 +10,7 @@ const dbConfig = defineConfig({
     sqlite: {
       client: 'better-sqlite3',
       connection: {
-        filename: app.tmpPath('db.sqlite3')
+        filename: `${env.get('DIR_NAME')}/db.sqlite3`/* app.tmpPath('db.sqlite3') */
       },
       useNullAsDefault: true,
       migrations: {
