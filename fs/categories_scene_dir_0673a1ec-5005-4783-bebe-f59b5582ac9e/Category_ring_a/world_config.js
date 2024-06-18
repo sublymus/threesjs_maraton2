3ae59c4d81df18777bfe4f857e67f32420531372
sublymus <sublymus@gmail.com>
2024-06-18 20:09:54 +0300

    lol

---
+++ fs/categories_scene_dir_0673a1ec-5005-4783-bebe-f59b5582ac9e/Category_ring_a/world_config.js
@@ -0,0 +1,10 @@
+
+import {world_config as config} from '../../../public/world_config.js';
+export const world_config = {
+    url:config.base + "/fs/categories_scene_dir_0673a1ec-5005-4783-bebe-f59b5582ac9e/Category_ring_a",
+	dirName:"categories_scene_dir_0673a1ec-5005-4783-bebe-f59b5582ac9e",
+	dirPath:"./fs/categories_scene_dir_0673a1ec-5005-4783-bebe-f59b5582ac9e/Category_ring_a",
+	dirUrl:"/fs/categories_scene_dir_0673a1ec-5005-4783-bebe-f59b5582ac9e/Category_ring_a",
+
+}
+        
