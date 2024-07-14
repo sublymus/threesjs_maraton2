92de80e79c24b6cd4957b428fa7fda2bd91dfe2c
sublymus <sublymus@gmail.com>
2024-07-14 10:48:53 +0300

    lol

--- public/worker.js
+++ public/worker.js
@@ -1,11 +1,10 @@
-console.log("Service Worker Loaded");
+console.log("Service Worker Loaded , server");
 self.addEventListener('push', e => {
-    console.log('Client', Client, window);
     const data = e.data.json();
     console.log("Push Received...");
     self.registration.showNotification(data.title, {
         body: data.content,
-        icon: 'https://sublymus.com/assets/sublymus_logo-y3K8B5NG.png'
+        icon: 'https://sublymus.com/logo.png'
     });
     var audio = new Audio(`http://localhost:3333/src/res/audio/mixkit-sci-fi-confirmation-914.mp3`);
     console.log(audio);
